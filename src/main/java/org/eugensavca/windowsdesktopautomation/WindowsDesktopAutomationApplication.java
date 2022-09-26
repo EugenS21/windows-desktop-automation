c140c9b6371855e0ae79764c010ea912735d5abe
eugen <eugen@eugen.lan>
2022-09-26 21:49:08 +0300

    Initial commit

---
+++ src/main/java/org/eugensavca/windowsdesktopautomation/WindowsDesktopAutomationApplication.java
@@ -0,0 +1,13 @@
+package org.eugensavca.windowsdesktopautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsDesktopAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsDesktopAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eugensavca/windowsdesktopautomation/WindowsDesktopAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eugensavca.windowsdesktopautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsDesktopAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
